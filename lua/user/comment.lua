0ac0f077b9a824e6c48c93c4ecb1bd69b8a672a7
adityamayukhsom <adi.kg2@gmail.com>
2023-11-27 13:07:51 +0530

    context_commentstring deprecated

--- lua/user/comment.lua
+++ lua/user/comment.lua
@@ -4,6 +4,12 @@ if not status_ok then
 	return
 end
 
+-- context_commentstring nvim-treesitter module is deprecated, use require('ts_context_commentstring').setup {} and set vim.g.skip_ts_context_commentstring_module = true to speed up loading instead.
+-- This feature will be removed in ts_context_commentstring version in the future
+-- This setting is related to treesitter
+require("ts_context_commentstring").setup({})
+vim.g.skip_ts_context_commentstring_module = true
+
 nvim_comment.setup({
 	-- Linters prefer comment and line to have a space in between markers
 	marker_padding = true,
