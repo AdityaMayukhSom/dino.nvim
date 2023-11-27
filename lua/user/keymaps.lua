9b2a6e82fd2950e8ccced13a4f96ef7df0d95691
adityamayukhsom <adi.kg2@gmail.com>
2023-11-27 13:26:01 +0530

    keymap: close current buffer

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -44,6 +44,9 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
+-- Close current buffer
+keymap("n", "<C-w>", ":Bdelete<CR>", opts)
+
 -- Insert --
 -- Press jk fast to enter
 keymap("i", "jj", "<ESC>", opts)
