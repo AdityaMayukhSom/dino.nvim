f06399fe5570be4a2655a72032e24b125fa09c64
Aditya Mayukh Som <adi.kg2@gmail.com>
2023-12-18 17:39:44 +0530

    added ayu theme and trouble.nvim, removed cp plugins

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -3,8 +3,14 @@ local fn = vim.fn
 -- Automatically install packer
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-	PACKER_BOOTSTRAP =
-		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+	PACKER_BOOTSTRAP = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
 	print("Installing packer close and reopen Neovim...")
 	vim.cmd([[packadd packer.nvim]])
 end
@@ -56,9 +62,10 @@ packer.init({
 return packer.startup(function(use)
 	-- My plugins here
 	use("wbthomason/packer.nvim") -- Have packer manage itself
-	use("nvim-lua/popup.nvim")  -- An implementation of the Popup API from vim in Neovim
-	use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
+	use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
+	use("nvim-lua/plenary.nvim") -- Useful lua functions used by lots of plugins
 	use("akinsho/toggleterm.nvim") -- useful terminal inside neovim
+	use("lewis6991/gitsigns.nvim")
 
 	-- plugins for bufferline
 	use("akinsho/bufferline.nvim")
@@ -66,15 +73,15 @@ return packer.startup(function(use)
 
 	-- colorscheme plugins
 	use("nyoom-engineering/oxocarbon.nvim") -- oxocarbon colourscheme for neovim
-	use("navarasu/onedark.nvim")         -- onedark colourscheme for neovim
-
-
+	-- use("navarasu/onedark.nvim")         -- onedark colourscheme for neovim
+	-- use("ayu-theme/ayu-dark")
+	use("Shatur/neovim-ayu")
 
 	-- cmp plugins
-	use("hrsh7th/nvim-cmp")      -- The completion plugin
-	use("hrsh7th/cmp-buffer")    -- buffer completions
-	use("hrsh7th/cmp-path")      -- path completions
-	use("hrsh7th/cmp-cmdline")   -- cmdline completions
+	use("hrsh7th/nvim-cmp") -- The completion plugin
+	use("hrsh7th/cmp-buffer") -- buffer completions
+	use("hrsh7th/cmp-path") -- path completions
+	use("hrsh7th/cmp-cmdline") -- cmdline completions
 	use("saadparwaiz1/cmp_luasnip") -- snippet completions
 	use("hrsh7th/cmp-nvim-lsp")
 	use("hrsh7th/cmp-nvim-lua")
@@ -82,11 +89,14 @@ return packer.startup(function(use)
 	-- status line configuration
 	use({
 		"nvim-lualine/lualine.nvim",
-		requires = { "nvim-tree/nvim-web-devicons", opt = true },
+		requires = {
+			"nvim-tree/nvim-web-devicons",
+			opt = true,
+		},
 	})
 
 	-- snippets
-	use("L3MON4D3/LuaSnip")          -- snippet engine
+	use("L3MON4D3/LuaSnip") -- snippet engine
 	use("rafamadriz/friendly-snippets") -- a bunch of snippets to use
 
 	-- plugins for nvim tree i.e. file explorer
@@ -94,7 +104,7 @@ return packer.startup(function(use)
 	use({
 		"nvim-tree/nvim-tree.lua",
 		requires = {
-			"nvim-tree/nvim-web-devicons", -- optional
+			"nvim-tree/nvim-web-devicons",
 		},
 	})
 
@@ -114,13 +124,17 @@ return packer.startup(function(use)
 	})
 
 	-- Folding
-	use({ "kevinhwang91/nvim-ufo", requires = "kevinhwang91/promise-async" })
+	use({
+		"kevinhwang91/nvim-ufo",
+		requires = "kevinhwang91/promise-async",
+	})
 
 	-- LSP
-	use("williamboman/mason.nvim")      -- simple to use language server installer
+	use("williamboman/mason.nvim") -- simple to use language server installer
 	use("williamboman/mason-lspconfig.nvim")
-	use("neovim/nvim-lspconfig")        -- enable LSP
+	use("neovim/nvim-lspconfig") -- enable LSP
 	use("jose-elias-alvarez/null-ls.nvim") -- for formatters and linters
+	-- automatically highlighting other uses of the word under the cursor using either LSP, Tree-sitter, or regex matching
 	use("RRethy/vim-illuminate")
 
 	-- Comment support
@@ -130,27 +144,30 @@ return packer.startup(function(use)
 	-- for creation of brackets in pair
 	use("windwp/nvim-autopairs") -- Autopairs, integrates with both cmp and treesitter
 
+	-- for tworkspace diagnostics
+	use("folke/trouble.nvim")
+
 	-- Used for markdown preview
-	use({
-		"iamcco/markdown-preview.nvim",
-		run = "cd app && npm install",
-		ft = { "markdown" },
-	})
+	-- use({
+	-- 	"iamcco/markdown-preview.nvim",
+	-- 	run = "cd app && npm install",
+	-- 	ft = { "markdown" },
+	-- })
 
 	-- used to configure rust with neovim
-	use("rust-lang/rust.vim")
-	use("simrat39/rust-tools.nvim")
+	-- use("rust-lang/rust.vim")
+	-- use("simrat39/rust-tools.nvim")
 
 	-- for running projects (especially for cp)
-	use("CRAG666/code_runner.nvim")
+	-- use("CRAG666/code_runner.nvim")
 	-- test case runner for competitive programming
-	use({
-		"xeluxee/competitest.nvim",
-		requires = "MunifTanjim/nui.nvim",
-		config = function()
-			require("competitest").setup()
-		end,
-	})
+	-- use({
+	-- 	"xeluxee/competitest.nvim",
+	-- 	requires = "MunifTanjim/nui.nvim",
+	-- 	config = function()
+	-- 		require("competitest").setup()
+	-- 	end,
+	-- })
 
 	-- to enable smooth scrolling
 	use("karb94/neoscroll.nvim")
@@ -158,6 +175,6 @@ return packer.startup(function(use)
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
-		require("packer").sync()
+		packer.sync()
 	end
 end)
